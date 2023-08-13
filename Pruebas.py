db8ab6e48762978f024c5b686f5edc900e71b44a
Jader <jadergav@hotmail.com>
2023-08-13 18:37:29 -0500

    Primer Commit

--- Pruebas.py
+++ Pruebas.py
@@ -1,7 +1,15 @@
-import mensajes
-name = input('Ingresa tu nombre: ')
-mensajes.display(name)
 
 
-import importlib
-importlib.reload(mensajes)
+
+
+'''
+nombre = "Guillermo Tell"
+print(nombre[0:5]) #Guill
+print(nombre[1:4]) #uil
+print(nombre[4:]) #ermo Tell
+print(nombre[-3]) #e
+print(nombre[-4:-1]) #Tel
+print(nombre[:]) #Guillermo Tell
+'''
+
+
