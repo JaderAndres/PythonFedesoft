929fe295a130c802b1736057028dde320d669f61
jagaviriab <jagaviriab@siesa.com>
2023-09-15 17:29:13 -0500

    Numpy 1

---
+++ Librerias/Numpy/Numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#array de una dimención
+
+a1 = np.array([1, 2, 3])
+print('-- Array de una dimención: --')
+print(a1)
+
+#array de dos dimenciones
+
+a2 = np.array([[1, 2, 3],[4, 5, 6]])
+print('-- Array de dos dimenciones: --')
+print(a2)
+
+#array de tres dimenciones
+
+a3 = np.array([[[1, 2, 3],[4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print('-- Array de tres dimenciones: --')
+print(a3)
