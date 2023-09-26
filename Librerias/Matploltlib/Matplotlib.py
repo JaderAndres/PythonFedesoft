ef604ff6ad566cc7bad4cc38eff81ba6ddaa4a63
jagaviriab <jagaviriab@siesa.com>
2023-09-26 18:54:03 -0500

    Matplotlib

---
+++ Librerias/Matploltlib/Matplotlib.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = np.linspace(0, 2 * np.pi, 2000)
+# y = np.sin(x)
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
+# plt.show()
+
+# x = np.array([1,2,3,4])
+# y = x*2
+# plt.plot(x, y)
+# plt.xlabel("Eje X")
+# plt.ylabel("Eje Y")
+# plt.title("Este es un ejemplo")
+# plt.show()
+
